a431f7b83540bf9fce929a58b44be211a8be35d2
Kirucoderanger <klemu@byupathway.edu>
2025-07-15 04:27:31 +0300

    week 3 assignment check deploy

---
+++ models/inventory-model.js
@@ -0,0 +1,52 @@
+const pool = require("../database/")
+
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+async function getClassifications(){
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+
+}
+
+/* ***************************
+for single Item
+async function getClassificationById(classification_id) {
+    const data = await pool.query(
+      "SELECT * FROM public.classification WHERE classification_id = $1 ORDER BY classification_name",
+       [classification_id]
+    )
+    return data.rows[0]
+  }
+    */
+//or
+/*******
+ * for multiple items
+  async function getClassifications() {
+    const data = await pool.query(
+      "SELECT * FROM public.classification ORDER BY classification_name"
+    )
+    return data.rows
+  }
+ */
+
+
+  /* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("get classifications by id error " + error)
+  }
+}
+
+
+module.exports = {getClassifications, getInventoryByClassificationId}
