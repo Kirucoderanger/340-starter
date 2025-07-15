89348afe7dc595d0c80660a8abbcbadf7447a499
Kirucoderanger <klemu@byupathway.edu>
2025-07-15 14:24:04 +0300

    render server

--- server.js
+++ server.js
@@ -68,12 +68,20 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+//const port = process.env.PORT
+//const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+//app.listen(port, () => {
+  //console.log(`app listening on ${host}:${port}`)
+//})
+
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+    
